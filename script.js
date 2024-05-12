f2e04475d290f43d9a8ed9695c6f75d04051584f
Vishal <100362137+Vishalsi23ngh@users.noreply.github.com>
2024-05-12 17:23:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function firstWord(s) {
   // your code here
+	let ans = "";
+	for(let i = 0; i < s.length(); i++){
+		let val = s.charAt(i);
+		if(!isNaN(val) && val != " "){
+			ans += val;
+		}else{
+			break;
+		}
+	}
+	return ans;
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
